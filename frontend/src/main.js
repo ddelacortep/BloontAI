a5f6c652726e6acb603b52b6fb54c130366756dc
ddelacortep <ddelacortep@gmail.com>
2026-02-09 19:43:21 +0100

    conf router

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,9 @@
 import './assets/main.css'
-
-import { createApp } from 'vue'
 import App from './App.vue'
+import { createApp } from 'vue'
+import router from './router'   
 
-import axios from axios
-
+const app = createApp(App)
+app.use(router)
 
 createApp(App).mount('#app')

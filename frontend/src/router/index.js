204db160804c85a2b88e1de3786593d9eff9a374
Anperezna <tete.perez.moreno@gmail.com>
2026-02-24 20:23:23 +0100

    Modleo de texto empezado

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ModeloImagenes from "../views/ModeloImagenes.vue";
+import ModeloTexto from "@/views/ModeloTexto.vue";
 
 
 
@@ -16,6 +17,11 @@ const router = createRouter({
             path: "/modelo-imagenes",
             name: "modelo-imagenes",
             component: ModeloImagenes,
+        },
+        {
+            path: "/modelo-texto",
+            name: "modelo-texto",
+            component: ModeloTexto
         }
     ]
 });

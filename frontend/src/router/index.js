2ab4740d5acb88dc7d25614fb45d3b3aef486aa0
danibarrf <daniel.barrufet@gmail.com>
2026-02-26 19:49:34 +0100

    cambios

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import ModeloImagenes from "../views/ModeloImagenes.vue";
+import ModeloAudio from "../views/ModeloAudio.vue";
 import SeleccionModelo from "@/views/SeleccionModelo.vue";
 
 
@@ -17,6 +18,11 @@ const router = createRouter({
             path: "/modelo-imagenes",
             name: "modelo-imagenes",
             component: ModeloImagenes,
+        },
+        {
+            path: "/modelo-audio",
+            name: "modelo-audio",
+            component: ModeloAudio,
         },
         {
             path: "/seleccion-modelo",
